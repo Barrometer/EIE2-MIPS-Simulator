93a6b1ed93247f43ec370802acbe47f60b3df05a
Edward Pickup <pickup.edward@gmail.com>
2017-11-06 18:13:21 +0000

    Skeleton for main function suggested

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,4 +1,24 @@
-int main(){
-	//I added this comment to test how ssh demon likes my vm
-	return 15;
+#include<iostream> //Required for std_in and std_out
+#include<cstdlib> //Required for exit
+#include<fstream> //Required to read from files
+
+
+
+
+
+int main(int argc, char* argv[]){ //Arg stuff added for command line inputs
+	/*
+
+	Suggested format for main
+	Store input binary in container: vector vs array? - container size is known
+		
+	while(running) - some condition to run, set at true on start
+		decode instruction
+		execute instruction
+		go to next instruction
+	Eventually terminate, and normal exit
+
+	*/	
+
+	return 0;
 }

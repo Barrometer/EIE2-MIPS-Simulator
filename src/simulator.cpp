ccab62b9b39616301e4d147457305ff8e7370ac0
Edward Pickup <pickup.edward@gmail.com>
2017-11-07 14:23:22 +0000

    More Skeletal development of main .cpp

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,16 +1,17 @@
 #include<iostream> //Required for std_in and std_out
 #include<cstdlib> //Required for exit
 #include<fstream> //Required to read from files
+#include<cstdint>//Required for uint_t type
 
 
-
+//void decode_inst(uint32_t instruction, int& opcode, int& funct_bit); // function which takes instruction by reference and opcode and funct_bit by value, and returns opcode and functiom
 
 
 int main(int argc, char* argv[]){ //Arg stuff added for command line inputs
 	/*
 
 	Suggested format for main
-	Store input binary in container: vector vs array? - container size is known
+	Store input binary in container: vector vs array? - container size is known, so probably array
 		
 	while(running) - some condition to run, set at true on start
 		decode instruction
@@ -19,6 +20,8 @@ int main(int argc, char* argv[]){ //Arg stuff added for command line inputs
 	Eventually terminate, and normal exit
 
 	*/	
+	
+
 
 	return 0;
 }

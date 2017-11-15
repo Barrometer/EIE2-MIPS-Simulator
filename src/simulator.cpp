d8deb3b5067dc16f754c27f2c059a6238bb69a83
Edward Pickup <pickup.edward@gmail.com>
2017-11-15 18:48:30 +0000

    Main loop and skeleton added. Skeleton should be finalised. Debug/test stuff present, check what you remove

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]){ //Arg stuff added for command line inputs
 	bin_in.close();
 	cout<<"DEBUG, binary stored in ROM, closed"<<endl;
 	
-	cout<<"TEST OPERATION, OUTPUTTING ROM"<<endl;
+	/*cout<<"TEST OPERATION, OUTPUTTING ROM"<<endl;
 	ofstream dummy_out;
 	dummy_out.open("dummy_out.txt");
 	// new indexer
@@ -61,6 +61,65 @@ int main(int argc, char *argv[]){ //Arg stuff added for command line inputs
 		
 		dummy_out<<ROM[c]<<endl;
 	}
+	*/
+	
+	
+	
+	
+	bool running = true; // this bool will be the controller on running
+	
+	
+	uint32_t prog_counter =0x10000000; // points to address ADDR_INSTR using its "actual" memory location
+	uint32_t prog_counter_next; //points to next instruction
+	
+	//ok, main loop
+	
+	while(running){
+		//default stuff
+		prog_counter_next = prog_counter +4; //default case, prog_counter_next could be updated further on
+		registers[0]=0; //just to make bloody sure
+		
+		//HERE YOU CAN DO STUFF	
+	
+		/*
+		fetch
+		decode
+		execute
+		
+		*/	
+	
+	
+		//these things should happen at the end of every loop. 
+		//Think before you put new stuff after this point!
+		
+		
+		//register 0 should always be zero
+		registers[0]=0; 
+		/*there are other ways of doing this, and we should probably code all functions to not update reg 0 with the result of an operation, but this is a good check
+		*/
+		
+		
+		//update program counter for next iteration of loop
+		cout<<"DEBUG, prog counter"<<endl;
+		cout<<prog_counter<<endl;
+		
+		prog_counter = prog_counter_next;
+		
+		cout<<"DEBUG, prog counter updated"<<endl;
+		cout<<prog_counter<<endl;
+		//prog_counter_next defaults to prog_counter+4, but could have been altered by instructions
+		
+		
+		
+		//now for some debug, remove as soon as stuff exists
+		cout<<"STUPID DEBUG, IF YOU SEE THIS THEN YOU LEFT THE LOOP YOU FOOL!"<<endl;
+		cout<<prog_counter<<endl;
+		cout<<prog_counter_next<<endl;
+		cout<<registers[0]<<endl;
+		cout<<"DEBUG LEAVE LOOP"<<endl;
+		running =false;
+	}
+	
 	cout<<"End of current code"<<endl;
 	
 	return 0;

399dc85d0ed8e565de63264e2ba6d644d6d0e627
Edward Pickup <pickup.edward@gmail.com>
2017-11-15 18:24:47 +0000

    Sort of a step back. Fixed makefile(leave like this, part of spec) Tested the ROM by loading a text file with the numbers from 0 to 4194304, and then outputting file as dummy

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-mips_simulator : src/simulator.cpp
-	g++ -std=c++11 -W -Wall src/simulator.cpp -o mips_simulator
+bin/mips_simulator : src/simulator.cpp
+	g++ -std=c++11 -W -Wall src/simulator.cpp -o bin/mips_simulator
--- src/simulator.cpp
+++ src/simulator.cpp
@@ -3,17 +3,77 @@
 #include<cstdlib> //Required for exit
 #include <stdint.h> //Required for uint_t type
 #include <vector>
-#include "r_type.cpp"
+//#include "r_type.cpp"
 
 using namespace std;
 
+//hideous global variable arrays
+//these arrays are put as global as it means they will not overflow in the stack, as they are part of the heap
+uint32_t RAM[16777216]={0};
+uint32_t ROM[4194304]={0};
+//all elements are intitalised to zero
 
 
 int main(int argc, char *argv[]){ //Arg stuff added for command line inputs
-
 	
+	//variables local to main
+	uint32_t registers[32] ={0};
+ 	// register 0 will always be 0. Add something to major loop enforcing this
+	
+	//Check for expected number of inputs
+	if(argc<2){
+		cout<<"Missing parameters"<<endl;
+		exit(-21); //relevant exit code
+	}
+	
+	
+	//Get binary
+	
+	ifstream bin_in;
+	
+	bin_in.open(argv[1]); //the first parameter will be name / location of bin
+	if(!(bin_in.is_open())){ //if not opened then return error
+		cout<<"Error, binary not found"<<endl; 
+		exit(-21);//relevant exit code
+	}
+	
+	//now to store in array
+	uint32_t a,b;
+	b =0; //b is indexer for array
+	while(bin_in>>a){
+		cout<<"DEBUG, storing element "<<a<<" at ROM index "<<b<<endl;
+		ROM[b]=a;
+		cout<<"DEBUG, ROM[b] = "<<ROM[b]<<endl;
+		b++;
+		if(b>4194304){//overflow case
+			cout<<"ERROR, binary is too large"<<endl;
+			exit(-21);
+		}
+	}
+	bin_in.close();
+	cout<<"DEBUG, binary stored in ROM, closed"<<endl;
+	
+	cout<<"TEST OPERATION, OUTPUTTING ROM"<<endl;
+	ofstream dummy_out;
+	dummy_out.open("dummy_out.txt");
+	// new indexer
+	for(uint32_t c =0; c<b;c++){
+		
+		dummy_out<<ROM[c]<<endl;
+	}
+	cout<<"End of current code"<<endl;
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 

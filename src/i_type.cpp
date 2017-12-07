345dbd8a2b056e67519057a7c6ed3020fac80086
Edward A Pickup <edp16@eews304-003.ee.ic.ac.uk>
2017-12-07 12:00:22 +0000

    Added invalid instruction catch

--- src/i_type.cpp
+++ src/i_type.cpp
@@ -17,7 +17,7 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 		return( source_reg & immediate);
 	}
 	//ADDI
-	if (operation == 8) {
+	else if (operation == 8) {
 		uint32_t copy_source = unsigned(source_reg);
 		int64_t test_overflow = 0;
 		int32_t test = 0;
@@ -39,11 +39,11 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 		//temp = source_reg + immediate;
 	}
 	//ADDIU
-	if (operation == 9){
+	else if (operation == 9){
 		return immediate + source_reg;
 	}
 	 // Yet to finish
-	if (operation == 32){ // Load Byte
+	else if (operation == 32){ // Load Byte
 			// In this case our Immediate is our Offset.
 			// EXCEPTIONS : TLB Refill, TLB Invalid and Address Error.
 			int32_t address = source_reg + immediate;
@@ -58,7 +58,7 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 		
 	}
 
-	if (operation == 36){ // Load Byte Unsigned
+	else if (operation == 36){ // Load Byte Unsigned
 			// In this case our Immediate is our Offset.
 			// EXCEPTIONS : TLB Refill, TLB Invalid and Address Error.
 			int32_t address = source_reg + immediate;
@@ -73,19 +73,19 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 		
 	} /// NEED TO CHECK ALL OF THESE
 	//ANDI
-	if (operation == 12){
+	else if (operation == 12){
 		return immediate & source_reg;
 	}
 	//ORI
-	if (operation == 13){
+	else if (operation == 13){
 		return immediate | source_reg;
 	}
 	//XORI
-	if (operation == 14){
+	else if (operation == 14){
 		return immediate ^ source_reg;
 	}
 
-	if (operation == 15){ // Load Upper Immediate
+	else if (operation == 15){ // Load Upper Immediate
 		temp = (immediate << 16);
 		temp = temp & 0xFFFF0000;
 		return temp;
@@ -94,7 +94,7 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 	
 	//// ALL OF THESE NEED TO BE CHECK PROPERLY
 	// Error check for address on the next 3 instructions
-	if (operation == 35){ // Load Word 
+	else if (operation == 35){ // Load Word 
 		int32_t address = ((source_reg + immediate)*4); // Since we have split the memory into from 32 bits to 8 bits;
 		if (address > 67108864){
 			// Return an address error;	
@@ -108,7 +108,7 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 			result += RAM[address+3];
 			}
 	}
-	if (operation == 40){ // Store Byte
+	else if (operation == 40){ // Store Byte
 		int32_t address = (source_reg + immediate)*4;
 		if (address > 67108864){
 			// Return an address error;
@@ -120,7 +120,7 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 			}
 	}
 
-	if (operation == 43){
+	else if (operation == 43){
 		int32_t address = (source_reg + immediate)*4;
 
 		if (address > 67108864){
@@ -133,13 +133,16 @@ int32_t i_type(int8_t operation, int32_t source_reg,int32_t dest_reg, int32_t im
 			RAM[address+3] = ((dest_reg) & 0xff);
 		}
 	}
-	if (operation == 10){ // SLTI
+	else if (operation == 10){ // SLTI
 		int16_t temp = immediate;
 		return source_reg < temp;
 	}
-	if (operation == 11){ // SLTIU
+	else if (operation == 11){ // SLTIU
 		uint16_t temp = immediate;
 		return source_reg < temp;
+	}
+	else{ //invalid instruction catch
+		exit(-12);
 	}
 }
 

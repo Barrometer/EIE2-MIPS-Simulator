837a33e2cd50124e7ed21df89af3e39c6cd5b65c
Edward A Pickup <edp16@eews304-003.ee.ic.ac.uk>
2017-12-07 12:55:29 +0000

    Inital testbench

---
+++ testbench.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+./$1 testbench/Ed_Test_1.bin
+
+if [ $? -eq 8 ] ; then
+	
+	echo -e "JALR_1,\t JALR,\t Pass,\t edp16"
+else
+	echo "Fail"
+
+fi
+
+./$1 testbench/Ed_Test_2.bin
+
+if [ $? -eq 15 ] ; then
+	
+	echo -e "ADDIU_1,\t ADDIU,\t Pass,\t edp16"
+else
+	echo "Fail"
+
+fi

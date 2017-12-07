4bb8efb2b8b5cb654a4e0c422637a8fdf0498d6f
Edward A Pickup <edp16@eews304-003.ee.ic.ac.uk>
2017-12-07 16:26:24 +0000

    Tim is so dumb

--- bettertestbench.sh
+++ bettertestbench.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-FILES = /testbench
+FILES ='/testbench'
 
 shopt -s nullglob # Check if directory exists
 
-find /testbench -name "expectedResults.txt" #is this needed?
+find /testbench -name"expectedResults.txt" #is this needed?
 mapfile -t expectArray < expectedResults.txt
 
 
 for file in $FILES ; do
-    count = 0
-    name = $(echo $filename | cut -f 1 -d ".")
-    instruction = $(echo $name | cut -f 1 -d "_")
-    author = $(echo $name | cut -f 2 -d "_")
+    count=0
+    name=$(echo $filename | cut -f 1 -d ".")
+    instruction=$(echo $name | cut -f 1 -d "_")
+    author=$(echo $name | cut -f 2 -d "_")
 
     -I "expectedResults.txt" // ignore answers file
     ./$1 $file
@@ -25,3 +25,5 @@ for file in $FILES ; do
 done
 
 shopt -u nullglob
+
+#fuck Tim

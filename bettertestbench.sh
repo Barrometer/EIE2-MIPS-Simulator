6f6802f72614e7830c2d76acb0d48cb93641a074
Tim <twutang17@gmail.com>
2017-12-08 14:18:42 +0000

    Update bettertestbench.sh

--- bettertestbench.sh
+++ bettertestbench.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+if [ $#!=1 ]; then
+	echo 'incorrect number of entry parameters'
+	exit
+fi
+
+if ! [ -d testbench ]; then
+	echo 'testbench does not exist'
+	exit
+fi
+
 FILES="/testbench"
 
-shopt -s nullglob # Check if directory exists
+answers="/testbench/expectedResults.txt"
+if ! [ -f "$answers" ]; then
+	echo 'answerfile not found'
+	exit
+fi
 
-#find /testbench -name"expectedResults.txt" #is this needed?
-mapfile -t expectArray < expectedResults.txt
+declare -a expectArray
+mapfile -t expectArray <"$answers"
 
-for file in $FILES ; do
-    count=0
-    name=$(echo $filename | cut -f 1 -d ".")
-    instruction=$(echo $name | cut -f 1 -d "_")
-    author=$(echo $name | cut -f 2 -d "_")
-
-    --ignore="expectedResults.txt" // ignore answers file
-    ./$1 $file
-    if [ $? -eq ${expectArray[$count]} ] ; then
-        printf "TestID: %s\nInstruction: %s\nStatus: %s\nAuthor" "$count" "instruction" "Pass" "$author"
-    else
-        printf "TestID: %s\nInstruction: %s\nStatus: %s\nAuthor" "$count" "$instruction" "Fail" "$author"
-    fi
-    let "count++"
+for file in $FILES; do
+	if [$file!="expectedResults.txt"]; then
+		count=0
+		name=$(echo $filename | cut -f 1 -d ".")
+		instruction=$(echo $name | cut -f 1 -d "_")
+		author=$(echo $name | cut -f 2 -d "_")
+		./$1 $file
+		if [ $? -eq ${expectArray[$count]} ]; then
+			printf "TestID: %s\nInstruction: %s\nStatus: %s\nAuthor" "$count" "instruction" "Pass" "$author"
+		else
+			printf "TestID: %s\nInstruction: %s\nStatus: %s\nAuthor" "$count" "$instruction" "Fail" "$author"
+		fi
+    let count++
+	fi
 done
-
-shopt -u nullglob

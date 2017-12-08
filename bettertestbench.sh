04ef990a73e5f8f449666d4fa0dbd0c632f42d48
Tim <twutang17@gmail.com>
2017-12-08 09:39:26 +0000

    Update bettertestbench.sh

--- bettertestbench.sh
+++ bettertestbench.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-FILES='/testbench'
+FILES="/testbench"
 
 shopt -s nullglob # Check if directory exists
 
 #find /testbench -name"expectedResults.txt" #is this needed?
 mapfile -t expectArray < expectedResults.txt
-
 
 for file in $FILES ; do
     count=0
@@ -14,16 +13,14 @@ for file in $FILES ; do
     instruction=$(echo $name | cut -f 1 -d "_")
     author=$(echo $name | cut -f 2 -d "_")
 
-    -I"expectedResults.txt" // ignore answers file
+    --ignore="expectedResults.txt" // ignore answers file
     ./$1 $file
     if [ $? -eq ${expectArray[$count]} ] ; then
         printf "TestID: %s\nInstruction: %s\nStatus: %s\nAuthor" "$count" "instruction" "Pass" "$author"
     else
         printf "TestID: %s\nInstruction: %s\nStatus: %s\nAuthor" "$count" "$instruction" "Fail" "$author"
     fi
-    let count += 1
+    let "count++"
 done
 
 shopt -u nullglob
-
-#fuck Tim
